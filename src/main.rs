b2b2a1d433d75f198ecaa3ac06d27f18d76303b1
Jim <jimsamuel50@gmail.com>
2025-02-19 00:47:45 +0100

    bincode de-serialisation of generated data

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 mod wal;
 mod memtable;
 
-use std::{collections::HashMap,sync::Mutex};
+use std::{collections::HashMap, fs::File, hash::Hash, sync::Mutex};
 
 fn main(){
     let mut sdb = Mutex::new(HashMap::<u64, u8>::new());
-    let _ = memtable::memtable::save(&mut sdb, 10, 0xa);
-    let _ = memtable::memtable::remove(&mut sdb, 10);
-    let _ = memtable::memtable::update(&mut sdb, 10, 0xb);
-    let _ = memtable::memtable::core_update(&mut sdb, 10, 0xb);
-    let _ = memtable::memtable::find(&mut sdb, 10);
-    let _= wal::wal::wal();
+    // let _ = memtable::memtable::save(&mut sdb, 10, 0xa);
+    // let _ = memtable::memtable::remove(&mut sdb, 10);
+    // let _ = memtable::memtable::update(&mut sdb, 10, 0xb);
+    // let _ = memtable::memtable::core_update(&mut sdb, 10, 0xb);
+    // let _ = memtable::memtable::find(&mut sdb, 10);
+
+    let F = File::create("wal.md").unwrap();
+    let ahead = wal::wal::WAL{file:F, file_name:"wal.md"};
+    let alog = ahead.new().write(&mut sdb, 10, 0xa);
+
+    let map: HashMap<u64, u8> = bincode::deserialize(&alog).unwrap();
+    let resv = map.iter().take(1);
+    print!("{:?}", resv);
 }
--- src/wal/wal.rs
+++ src/wal/wal.rs
@@ -1,8 +1,11 @@
-use std::{collections::HashMap, fs::{File, OpenOptions}, io::{Read, Write}, sync::Mutex};
+use std::{collections::HashMap, fs::{self, File, OpenOptions}, io::{Read, Write}, sync::Mutex};
 
 use bincode;
 
 use crate::memtable::memtable;
+
+#[allow(non_camel_case_types)]
+type string = String;
 
 pub struct WAL{
     pub file: File,
@@ -11,21 +14,21 @@ pub struct WAL{
 
 impl WAL {
     #[allow(non_snake_case)]
-    pub fn new(&self,Log: &'static str) -> WAL {
-        let file = OpenOptions::new().create(true).append(true).open(Log).unwrap();
-        WAL {file, file_name:&Log}
+    pub fn new(&self) -> WAL {
+        let file = OpenOptions::new().create(true).append(true).open(self.file_name).unwrap();
+        WAL {file, file_name:self.file_name}
     }
 
     #[allow(non_snake_case)]
     pub fn write(&self, db: &mut Mutex<HashMap<u64,u8>>, k: u64, v:u8) -> Vec<u8> {
         let mut wal_map = HashMap::<u64, u8>::new();
-        let wal_file = self.new(self.file_name);
+        let wal_file = self.new();
         let mut wal_file = File::create(wal_file.file_name).unwrap();
 
         wal_map.insert(k, v);
         let serialised = bincode::serialize(&wal_map).expect("serialisation failed");
-        wal_file.write(&serialised);
-        memtable::save(db, k, v);
+        wal_file.write(&serialised).expect("write error");
+        memtable::save(db, k, v).expect("cannot save to memtable");
         serialised
     }
 
@@ -35,5 +38,19 @@ impl WAL {
         let mut db_lock = db.lock().expect("failed to lock mutex");
         db_lock.extend(deserialised);
         Ok(())
+    }
+
+    pub fn wal(&self){
+        let mut walfile = File::open(self.file_name).expect("cannot open file");
+        let mut waloutput = string::new();
+        walfile.read_to_string(&mut waloutput).expect("reading error");
+        for line in waloutput.lines(){
+            let line = line.trim();
+            print!("{}\n", line);
+        }
+    }
+
+    pub fn delete(&self){
+        fs::remove_file(self.file_name).expect("error removing file");
     }
 }
