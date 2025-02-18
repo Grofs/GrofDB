2dee84959c4e8187ef996b1ab7dc087b42466fe9
Jim <jimsamuel50@gmail.com>
2025-02-19 00:57:49 +0100

    memtable and wal smoke tests with real numbers

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod wal;
 mod memtable;
 
-use std::{collections::HashMap, fs::File, hash::Hash, sync::Mutex};
+use std::{collections::HashMap, fs::File, hash::Hash, mem, sync::Mutex};
 
 fn main(){
     let mut sdb = Mutex::new(HashMap::<u64, u8>::new());
-    // let _ = memtable::memtable::save(&mut sdb, 10, 0xa);
-    // let _ = memtable::memtable::remove(&mut sdb, 10);
-    // let _ = memtable::memtable::update(&mut sdb, 10, 0xb);
-    // let _ = memtable::memtable::core_update(&mut sdb, 10, 0xb);
-    // let _ = memtable::memtable::find(&mut sdb, 10);
+    let _ = memtable::memtable::save(&mut sdb, 10, 0xa);
+    let _ = memtable::memtable::update(&mut sdb, 10, 0xb);
+    let _ = memtable::memtable::core_update(&mut sdb, 10, 0xb);
+    let memtable_search = memtable::memtable::find(&mut sdb, 10).unwrap();
+    println!("{:?}", memtable_search);
 
     let F = File::create("wal.md").unwrap();
     let ahead = wal::wal::WAL{file:F, file_name:"wal.md"};
