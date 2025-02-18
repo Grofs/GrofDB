cfde42a29caddbced1daf9534661e5aa977284dc
Jim <jimsamuel50@gmail.com>
2025-02-18 21:10:02 +0100

    Data recovery: file tolerance recovery replay mechanism if memtable data is lost

--- src/wal/wal.rs
+++ src/wal/wal.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs::{File, OpenOptions}, io::Write, sync::Mutex};
+use std::{collections::HashMap, fs::{File, OpenOptions}, io::{Read, Write}, sync::Mutex};
 
 use bincode;
 
@@ -17,16 +17,23 @@ impl WAL {
     }
 
     #[allow(non_snake_case)]
-    pub fn write(&self, db: &mut Mutex<HashMap<u64,u8>>, k: &str, v:&str) {
-        let mut wal_map = HashMap::<&str, &str>::new();
+    pub fn write(&self, db: &mut Mutex<HashMap<u64,u8>>, k: u64, v:u8) -> Vec<u8> {
+        let mut wal_map = HashMap::<u64, u8>::new();
         let wal_file = self.new(self.file_name);
         let mut wal_file = File::create(wal_file.file_name).unwrap();
 
         wal_map.insert(k, v);
         let serialised = bincode::serialize(&wal_map).expect("serialisation failed");
         wal_file.write(&serialised);
-        let Fku64 = k.parse::<u64>().expect("invalid u64 conversion");
-        let Fku8 = v.parse::<u8>().expect("invalid value conversion to u8");
-        memtable::save(db, Fku64, Fku8);
+        memtable::save(db, k, v);
+        serialised
+    }
+
+    #[allow(non_snake_case)]
+    pub fn replay(&self, db: &mut Mutex<HashMap<u64, u8>>, data: &[u8])->Result<(), bincode::Error>{
+        let deserialised: HashMap<u64, u8> = bincode::deserialize(data)?;
+        let mut db_lock = db.lock().expect("failed to lock mutex");
+        db_lock.extend(deserialised);
+        Ok(())
     }
 }
