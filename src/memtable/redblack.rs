54e47141007dd10b57c6dc5bbddcdf8a43c4e68b
Jim <jimsamuel50@gmail.com>
2025-02-24 17:28:03 +0100

    fault violations handling on red black trees and uncles

--- src/memtable/redblack.rs
+++ src/memtable/redblack.rs
@@ -71,4 +71,33 @@ impl<T: Ord> RedBlackTree<T> {
         }
         node
     }
+
+    // fix some violations that might occur in the course of
+    // creating the tree such as consecutive red uncles and 
+    // children nodes being all red
+    pub fn fix_violations<T>(&mut self, node:&mut Box<Node<T>>){
+        if let Some(ref mut left) = node.left {
+            if left.color == Color::Red{
+                // apply recoloring or rotations as needed
+                // if left and right is red, then recolor them
+                // to black and make the parent to be black
+                if let Some(ref mut right) = node.right{
+                    // uncle is red here in this case
+                    if right.color == Color::Red {
+                        node.color = Color::Red;
+                        left.color = Color::Black;
+                        right.color = Color::Black;
+                    } else {
+                        // uncle is black in this case
+                        // incomplete implementation
+                        if let Some(ref mut left_left) = left.left {
+                            if left_left.color == Color::Black{
+                                left.color = Color::Red;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
